8e6095e3964288a1378b3fb3754325bdc78b8603
book-end <85774845+book-end@users.noreply.github.com>
2023-10-20 22:49:45 -0700

    add context

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views import generic
 from .models import Item
@@ -6,6 +7,10 @@ from .models import Item
 class MenuList(generic.ListView):
     queryset = Item.objects.order_by("date_created") # connect Item model to menu list view
     template_name = "index.html"
+
+    def get_context_data(self):
+        context = {"item": "Pizza"}
+        return context
 
 class MenuItemDescription(generic.DetailView):
     # model is predefined var name
