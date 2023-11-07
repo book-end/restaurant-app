12bc1928f8a51df54e673c734f84cec33e1478c7
book-end <85774845+book-end@users.noreply.github.com>
2023-11-06 16:27:57 -0800

    added dynamic urls for menu items

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MenuList.as_view(), name='home') # homepage
+    path('', views.MenuList.as_view(), name='home'),
+    path('item/<int:pk>/', views.MenuItemDescription.as_view(), name="menu_item") # int primary key pk for each item id, for dynamic urls
 ]
